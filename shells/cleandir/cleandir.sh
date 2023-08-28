69a0ff4fbb3d78863119419db9afc961fb10eeeb
Ramo Mislimi <ramo.mislimi@knowit.se>
2023-08-28 21:37:45 +0200

    add: cleandir

---
+++ shells/cleandir/cleandir.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+function cleandir() {
+  if [[ -z "$1" ]]; then
+    echo "Usage: cleandir <filename>"
+    return 1
+  fi
+
+  find . -name "$1" -exec printf "\033[32m[Removing]\033[0m %s\n" {} \; -exec rm {} \;
+}
